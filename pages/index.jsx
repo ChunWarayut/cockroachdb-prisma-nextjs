7925fc3dc683f1ea327eaf3724e31c68b78a885f
ChunWarayut <warayut2555@gmail.com>
2022-11-12 14:46:51 +0700

    chang title

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>PROJECT TIMELINE | BlueC ERP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
